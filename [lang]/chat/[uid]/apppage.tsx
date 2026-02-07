3ab6e083f44d9428dd8e3f3bbb90a0c7abf21eba
shuhratovxasan-spec <shuhratov.xasan@gmail.com>
2026-02-07 16:29:35 -0500

    Delete [lang]/chat/[uid] directory

--- [lang]/chat/[uid]/apppage.tsx
+++
@@ -1,9 +0,0 @@
-// app/[lang]/chat/[uid]/page.tsx
-import ChatClient from "@/components/ChatClient";
-
-export default async function Page(props: any) {
-  const p = await props.params;
-  const lang = p?.lang === "ru" ? "ru" : "en";
-  const otherUid = String(p?.uid || "");
-  return <ChatClient lang={lang} otherUid={otherUid} />;
-}
