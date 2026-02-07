e3cd548c7d1152e762b0b4acdbb5767fbc3ea011
shuhratovxasan-spec <shuhratov.xasan@gmail.com>
2026-02-07 16:32:12 -0500

    Add files via upload

---
+++ app/[lang]/page.tsx
@@ -0,0 +1,8 @@
+export default function ChatsPage() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Chats</h1>
+      <p>This route exists now. Next step: show chat list here.</p>
+    </div>
+  );
+}
